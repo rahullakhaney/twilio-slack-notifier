e928e2f9bc6413403f956b3ab051fe31a50fa5da
Łukasz Ozimek <lukasz.ozimek@netguru.co>
2017-04-21 14:47:54 +0200

    update SlackNotifier service specs

--- spec/services/slack_notifier_spec.rb
+++ spec/services/slack_notifier_spec.rb
@@ -6,7 +6,6 @@ describe SlackNotifier do
   let(:notifier) do
     described_class.new(channel: '#channel', twilio_name: 'twilio', slack_token: 'ABC123')
   end
-
   let(:slack_client) { double('slack_client') }
 
   subject { notifier }
@@ -100,10 +99,28 @@ describe SlackNotifier do
 
   describe '#answered_call_notification' do
     context 'when client is known' do
-      it 'sends the message' do
-        allow_any_instance_of(Slack::Client).to receive(:answered_call_notification)
-        expect(slack_client).to receive(:chat_postMessage)
-        subject.answered_call_notification('client:twilio')
+      subject { notifier.answered_call_notification('client:twilio') }
+
+      it 'sends the message to proper channel' do
+        expect(slack_client)
+          .to receive(:chat_postMessage)
+          .with(
+            hash_including(
+              channel: '#channel'
+            )
+          )
+        subject
+      end
+
+      it 'sends the message with proper text' do
+        expect(slack_client)
+          .to receive(:chat_postMessage)
+          .with(
+            hash_including(
+              text: 'Call answered :point_up:'
+            )
+          )
+        subject
       end
     end
 
@@ -115,7 +132,7 @@ describe SlackNotifier do
       end
     end
 
-    context 'when from is missing' do
+    context 'when parameter is missing' do
       it 'raises error' do
         expect { subject.answered_call_notification }.to raise_error(ArgumentError)
       end
@@ -123,10 +140,75 @@ describe SlackNotifier do
   end
 
   describe '#finished_call_notification' do
-    it 'sends the message' do
-      allow_any_instance_of(Slack::Client).to receive(:finished_call_notification)
-      expect(slack_client).to receive(:chat_postMessage)
-      subject.finished_call_notification
+    before { allow(slack_client).to receive(:chat_postMessage) }
+
+    subject { notifier.finished_call_notification }
+
+    it 'sends the message to proper channel' do
+      expect(slack_client)
+        .to receive(:chat_postMessage)
+        .with(
+          hash_including(
+            channel: '#channel'
+          )
+        )
+      subject
+    end
+
+    it 'sends the message with proper notification' do
+      expect(slack_client)
+        .to receive(:chat_postMessage)
+        .with(
+          hash_including(
+            text: 'Call finished :end:'
+          )
+        )
+      subject
+    end
+  end
+
+  describe '#last_message_recording_notification' do
+    let(:recording) { double }
+
+    subject { notifier.last_message_recording_notification(recording: recording) }
+
+    before do
+      allow_any_instance_of(Slack::Client).to receive(:last_message_recording_notification)
+      allow(recording).to receive(:duration)
+      allow(recording).to receive(:mp3)
+      allow(slack_client).to receive(:chat_postMessage)
+    end
+
+    it 'sends the message to proper channel' do
+      expect(slack_client)
+        .to receive(:chat_postMessage)
+        .with(
+          hash_including(
+            channel: '#channel'
+          )
+        )
+      subject
+    end
+
+    it 'sends the message with proper text' do
+      expect(slack_client)
+        .to receive(:chat_postMessage)
+        .with(
+          hash_including(
+            text: 'Last message recording is ready for you!'
+          )
+        )
+      subject
+    end
+
+    it 'gets the recording duration' do
+      expect(recording).to receive(:duration)
+      subject
+    end
+
+    it 'sends the recording in mp3 format' do
+      expect(recording).to receive(:mp3)
+      subject
     end
   end
 end
