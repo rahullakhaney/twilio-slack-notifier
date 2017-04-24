35f3eb937edac7e51577b7c6205d396f3c422a07
Łukasz Ozimek <lukasz.ozimek@netguru.co>
2017-04-24 15:47:24 +0200

    improve AppConfig implementation

--- config/app_config.rb
+++ config/app_config.rb
@@ -3,13 +3,21 @@
 require 'yaml'
 
 class AppConfig < Sinatra::Base
-  @config = OpenStruct.new(YAML.safe_load(File.read('config/config.yml')))
+  @file = YAML.safe_load(File.read('config/config.yml'))
+  @config = OpenStruct.new(@file)
 
   def self.method_missing(name)
-    if @config[name.to_s].is_a?(Hash)
-      OpenStruct.new(@config[name.to_s])
-    else
-      @config[name.to_s]
+    define_singleton_method(name) do
+      if @config[name.to_s].is_a?(Hash)
+        OpenStruct.new(@config[name.to_s])
+      else
+        @config[name.to_s]
+      end
     end
+    send(name.to_sym)
+  end
+
+  def self.respond_to_missing?(name, include_private = false)
+    @file.to_s.match(/"%{name.to_s}"/) || super
   end
 end
